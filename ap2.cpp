aac6341ce670482bd3b611aa1abf9569a26585ee
OmidZJ <zamanijamshidi.omid@gmail.com>
2024-10-23 03:56:41 +0330

    some changes in code

--- ap2.cpp
+++ ap2.cpp
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-map<string,vector<string>> cars;
+
 
 vector<string> split(const string &str, char delimiter = ',') {
     vector<string> result;
@@ -30,14 +30,14 @@ vector<string> split(const string &str, char delimiter = ',') {
     return result;
 }
 
-void readFromFile(const string& filename) {
+map<string,vector<string>> readFromFile(const string& filename,map<string,vector<string>> maptemp) {
     ifstream file(filename);
     string line;
 
     // Check if the file is open
     if (!file.is_open()) {
         cerr << "Error opening file: " << filename << endl;
-        return;
+        return {};
     }
 
     // Read the file line by line
@@ -47,26 +47,43 @@ void readFromFile(const string& filename) {
         for(auto i = row.begin()+1; i!=row.end();i++){
             temp.push_back(*i);
         }
-        cars[row[0]] = temp;
+        maptemp[row[0]] = temp;
     }
 
-    for(auto name:cars){
+    for(auto name:maptemp){
             cout << name.first << " ";
             for(auto value:name.second){
                 cout << value << " ";
             }
             cout << endl;    
     }
-        cars.clear();
+        //cars.clear();
 
     file.close(); // Close the file
+    return maptemp;
 }
 
-void request_spot(){
-    cout << "\n";
+void request_spot(string temp,map<string,vector<string>> car, map<string,vector<string>> park, map<string,vector<string>> cost){
+    string size,type,id, static_price, price_per_day;
+
+    size = car[temp][0];
+    for(auto place:park){
+        if ((place.second[0] == size) and (place.second[2] == "0")){
+            id = place.first;
+            type = place.second[1];
+            static_price = cost[size][0];
+            price_per_day = cost[size][1];
+            cout << id << ": " << type << " " << static_price << " " << price_per_day << "\n";
+        }
+    }
+
 }
-void assign_spot(){
-    cout << "\n";
+
+void assign_spot(string temp, map<string,vector<string>> park){
+    if(park[temp][2]=="0"){
+        park[temp][2] = "1";
+        cout << "Spot " << temp << " is occupied now." << endl;
+    }
 }
 
 void checkout(){
@@ -74,17 +91,26 @@ void checkout(){
 }
 
 int main(){
-    string command;
-    readFromFile("cars");
-    readFromFile("parking");
-    readFromFile("costs");
+    string command,temp;
+    map<string,vector<string>> cars,parking,costs;
+    
+    cars = readFromFile("cars",cars);
+
+    parking = readFromFile("parking",parking);
+    for(auto i:parking){
+        parking[i.first].push_back("0"); // for empty
+    }
+    
+    costs = readFromFile("costs",costs);
 
     while (cin >> command) {
         if (command == "request_spot") {
-            request_spot();
+            cin >> temp;
+            request_spot(temp, cars, parking, costs);
             cout << "\n";
         } else if (command == "assign_spot") {
-            assign_spot();
+            cin >> temp;
+            assign_spot(temp,parking);
             cout << "\n";
         } else if (command == "checkout") {
             checkout();

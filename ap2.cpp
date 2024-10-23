22cb03749cf80c62f0ad462dfe43c3eb28bfb0a2
OmidZJ <zamanijamshidi.omid@gmail.com>
2024-10-23 05:17:59 +0330

    some changes in code

--- ap2.cpp
+++ ap2.cpp
@@ -8,10 +8,10 @@
 using namespace std;
 const char delimiter = ',';
 
-map<string,vector<string>> cars,parking,costs;
+map<string, vector<string>> cars, parking, costs;
 
 vector<string> split(const string &str);
-map<string,vector<string>> readFromFile(const string& filename,map<string,vector<string>> maptemp);
+map<string, vector<string>> readFromFile(const string &filename, map<string, vector<string>> maptemp);
 void request_spot(string temp);
 void assign_spot(string temp);
 void checkout(string temp);
@@ -19,96 +19,117 @@ void pass_time(string temp);
 void update_costs();
 void initializing();
 
-int main(){
-    string command,temp;
-    
+int main()
+{
+    string command, temp;
+
     initializing();
 
-    while (cin >> command) {
-        if (command == "request_spot") {
+    while (cin >> command)
+    {
+        if (command == "request_spot")
+        {
             cin >> temp;
             request_spot(temp);
-
-        } else if (command == "assign_spot") {
+        }
+        else if (command == "assign_spot")
+        {
             cin >> temp;
             assign_spot(temp);
-
-        } else if (command == "checkout") {
+        }
+        else if (command == "checkout")
+        {
             cin >> temp;
             checkout(temp);
-
-        } else if (command == "pass_time") {
+        }
+        else if (command == "pass_time")
+        {
             cin >> temp;
             pass_time(temp);
-
-        } else if (command == "exit") {
+        }
+        else if (command == "exit")
+        {
             break;
         }
     }
     return 0;
 }
 
-
-vector<string> split(const string &str) {
+vector<string> split(const string &str)
+{
     vector<string> result;
     string temp;
 
-    for (auto s = str.begin(); s != str.end(); ++s) {
-        if (*s == delimiter) {
+    for (auto s = str.begin(); s != str.end(); ++s)
+    {
+        if (*s == delimiter)
+        {
             result.push_back(temp);
             temp.clear();
-        } else {
+        }
+        else
+        {
             temp += *s;
         }
     }
 
     // Add the last part after the final delimiter (if any)
-    if (!temp.empty()) {
+    if (!temp.empty())
+    {
         result.push_back(temp);
     }
 
     return result;
 }
 
-map<string,vector<string>> readFromFile(const string& filename,map<string,vector<string>> maptemp) {
+map<string, vector<string>> readFromFile(const string &filename, map<string, vector<string>> maptemp)
+{
     ifstream file(filename);
     string line;
 
     // Check if the file is open
-    if (!file.is_open()) {
+    if (!file.is_open())
+    {
         cerr << "Error opening file: " << filename << endl;
         return {};
     }
 
     // Read the file line by line
-    while (getline(file, line)) {
-        vector<string> row,temp;
+    while (getline(file, line))
+    {
+        vector<string> row, temp;
         row = split(line);
-        for(auto i = row.begin()+1; i!=row.end();i++){
+        for (auto i = row.begin() + 1; i != row.end(); i++)
+        {
             temp.push_back(*i);
         }
         maptemp[row[0]] = temp;
     }
 
-    for(auto name:maptemp){
-            cout << name.first << " ";
-            for(auto value:name.second){
-                cout << value << " ";
-            }
-            cout << endl;    
+    for (auto name : maptemp)
+    {
+        cout << name.first << " ";
+        for (auto value : name.second)
+        {
+            cout << value << " ";
+        }
+        cout << endl;
     }
-        //cars.clear();
+    // cars.clear();
 
     file.close(); // Close the file
     return maptemp;
 }
 
-void request_spot(string temp){
-    string size,type,id, static_price, price_per_day;
+void request_spot(string temp)
+{
+    string size, type, id, static_price, price_per_day;
 
     size = cars[temp][0];
-    for(auto place:parking){
-        if ((place.second[0] == size) and (place.second[2] == "0")){
+    for (auto place : parking)
+    {
+        if ((place.second[0] == size) and (place.second[2] == "0"))
+        {
             id = place.first;
             type = place.second[1];
             static_price = costs[size][0];
@@ -116,53 +137,63 @@ void request_spot(string temp){
             cout << id << ": " << type << " " << static_price << " " << price_per_day << "\n";
         }
     }
-
 }
 
-void assign_spot(string temp){
-    if(parking[temp][2]=="0"){
+void assign_spot(string temp)
+{
+    if (parking[temp][2] == "0")
+    {
         parking[temp][2] = "1";
         cout << "Spot " << temp << " is occupied now." << endl;
     }
     update_costs();
 }
 
-void checkout(string temp){
-    if(parking[temp][2]=="1"){
+void checkout(string temp)
+{
+    if (parking[temp][2] == "1")
+    {
         parking[temp][2] = "0";
         cout << "Spot " << temp << " is free now." << endl;
         cout << "Total cost: " << parking[temp][3] << endl;
     }
 }
 
-void pass_time(string temp){
-    
-    for (auto i:parking){
-        if(parking[i.first][2]=="1"){
+void pass_time(string temp)
+{
+
+    for (auto i : parking)
+    {
+        if (parking[i.first][2] == "1")
+        {
             parking[i.first][4] = to_string(stoi(parking[i.first][4]) + stoi(temp));
         }
     }
     update_costs();
 }
 
-void update_costs(){
-    for(auto i:parking){
-        if (i.second[2] == "1"){
-            parking[i.first][3] = to_string(stoi(costs[i.second[0]][0]) + stoi(costs[i.second[0]][1])*stoi(i.second[4]));
+void update_costs()
+{
+    for (auto i : parking)
+    {
+        if (i.second[2] == "1")
+        {
+            parking[i.first][3] = to_string(stoi(costs[i.second[0]][0]) + stoi(costs[i.second[0]][1]) * stoi(i.second[4]));
         }
     }
 }
 
-void initializing(){
-    cars = readFromFile("cars",cars);
+void initializing()
+{
+    cars = readFromFile("cars", cars);
 
-    parking = readFromFile("parking",parking);
-    for(auto i:parking){
+    parking = readFromFile("parking", parking);
+    for (auto i : parking)
+    {
         parking[i.first].push_back("0"); // for empty
         parking[i.first].push_back("0"); // for costs
         parking[i.first].push_back("0"); // for days
     }
-    
-    costs = readFromFile("costs",costs);
-}
 
+    costs = readFromFile("costs", costs);
+}
